8bec0f5e9652454f7d81c39512ae233e2100d6c3
Abhishek Nagpal <dev.abhisheknagpal@gmail.com>
2026-05-10 18:58:18 +0530

    flake8 changes in config file

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
     print(model_config)
-    OPENAI_API_KEY:str
-    GEMINI_API_KEY:str
-    ANTHROPIC_API_KEY:str
-    GROQ_API_KEY:str
-    HUGGINGFACEHUB_API_TOKEN:str
+    OPENAI_API_KEY: str
+    GEMINI_API_KEY: str
+    ANTHROPIC_API_KEY: str
+    GROQ_API_KEY: str
+    HUGGINGFACEHUB_API_TOKEN: str
+
 
 settings = Settings()
